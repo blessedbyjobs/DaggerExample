44e09b86d0d08a4a200e65f4947ef94038226a04
ryabkov-surf <ryabkov@surfstudio.ru>
2020-04-02 12:49:27 +0300

    Добавлен ActivityScope

--- app/src/main/java/com/example/daggerexample/di/ActivityComponent.kt
+++ app/src/main/java/com/example/daggerexample/di/ActivityComponent.kt
@@ -1,6 +1,12 @@
 package com.example.daggerexample.di
 
 import dagger.Component
+import javax.inject.Scope
 
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
+
+@ActivityScope
 @Component(dependencies = [AppComponent::class])
 interface ActivityComponent
