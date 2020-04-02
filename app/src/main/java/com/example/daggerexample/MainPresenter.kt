b08378f595cc314346155c22da4d39020696ef79
ryabkov-surf <ryabkov@surfstudio.ru>
2020-04-02 16:25:39 +0300

    Добавлен PresenterInterface

--- app/src/main/java/com/example/daggerexample/MainPresenter.kt
+++ app/src/main/java/com/example/daggerexample/MainPresenter.kt
@@ -1,18 +1,23 @@
 package com.example.daggerexample
 
 import android.app.Application
+import android.util.Log
 import com.example.daggerexample.di.ActivityScope
 import com.example.daggerexample.di.App
 import com.example.daggerexample.di.DaggerActivityComponent
 import javax.inject.Inject
 
 @ActivityScope
-class MainPresenter @Inject constructor(application: Application) {
+class MainPresenter @Inject constructor(application: Application) : MainPresenterInterface {
 
     init {
         DaggerActivityComponent.builder()
             .appComponent((application as App).appComponent)
             .build()
             .inject(this)
+    }
+
+    override fun func() {
+        Log.i("Presenter", "Hey!")
     }
 }
---
+++ app/src/main/java/com/example/daggerexample/MainPresenterInterface.kt
@@ -0,0 +1,5 @@
+package com.example.daggerexample
+
+interface MainPresenterInterface {
+    fun func()
+}
